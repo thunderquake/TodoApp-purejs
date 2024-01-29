d9154e696bbf9cf83f86af8bd0502ed2483a0c46
Dmytriieva Anna <loooverrr12@gmail.com>
2024-01-29 21:55:28 +0200

    fix: fixed getDate funct

--- src/helpers/getDate.js
+++ src/helpers/getDate.js
@@ -1,27 +1,13 @@
-export function getDate(d) {
-  let day, month, year, result, dateSplitted;
-
-  result = d.match("[0-9]{2}([-/ .])[0-9]{2}[-/ .][0-9]{4}");
-  if (result) {
-    dateSplitted = result[0].split(result[1]);
-    day = dateSplitted[0];
-    month = dateSplitted[1];
-    year = dateSplitted[2];
-  } else {
-    result = d.match("[0-9]{4}([-/ .])[0-9]{2}[-/ .][0-9]{2}");
-    if (result) {
-      dateSplitted = result[0].split(result[1]);
-      day = dateSplitted[2];
-      month = dateSplitted[1];
-      year = dateSplitted[0];
-    }
+export function getDate(noteContent) {
+  const dateFormats =
+    /(((0[1-9]|[12][0-9]|3[01])([\/.-])(0[13578]|10|12)([\/.-])(\d{4}))|((0[1-9]|[12][0-9]|30)([\/.-])(0[469]|11)([\/.-])(\d{4}))|((0[1-9]|1[0-9]|2[0-8])([\/.-])(02)([\/.-])(\d{4}))|((29)([\/.-])(02)([\/.-])([02468][048]00))|((29)(\/)(02)(\/)([13579][26]00))|((29)([\/.-])(02)([\/.-])([0-9][0-9]0[48]))|((29)([\/.-])(02)([\/.-])([0-9][0-9][2468][048]))|((29)([\/.-])(02)([\/.-])([0-9][0-9][13579][26])))/g;
+  const dates = [];
+  if (noteContent.match(dateFormats)) {
+    noteContent.match(dateFormats).forEach((date) => {
+      let formattedDate = date.replaceAll("-", "/");
+      formattedDate = date.replaceAll(".", "/");
+      dates.push(formattedDate);
+    });
   }
-
-  if (month > 12) {
-    let aux = day;
-    day = month;
-    month = aux;
-  }
-
-  return year && month && day ? day + "/" + month + "/" + year : "";
+  return dates.join(", ");
 }
