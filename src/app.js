a1e13df806a744a3d6f2d260c9ad2a4eae313ace
Dmytriieva Anna <loooverrr12@gmail.com>
2024-01-29 19:18:12 +0200

    fix: fixed funct names, moved them to components

--- src/app.js
+++ src/app.js
@@ -7,21 +7,3 @@ export function todosPage() {
     return element;
 }
 
-export function createCont(child) {
-    const container = document.createElement("div");
-    container.classList.add("bg-stone-50/60", "pt-8", "flex", "overflow-y-auto", "w-full");
-    container.classList.add("container", "mx-auto", "rounded-lg", "shadow-lg", "max-h-80");
-    container.appendChild(child);
-    document.body.appendChild(container);
-    return container;
-}
-
-export function create3Column(child, child1, child2) {
-    const container = document.createElement("div");
-    container.classList.add("grid", "mx-auto", "grid-cols-3","p-6", "justify-center", "place-content-center",  "w-2/6", "gap-10");
-    document.body.appendChild(container)
-    container.appendChild(child);
-    container.appendChild(child1);
-    container.appendChild(child2);
-    return container;
-}
