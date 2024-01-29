d995ae7cb43820cd9d2029935a5aa069b1573ae9
Dmytriieva Anna <loooverrr12@gmail.com>
2024-01-29 19:28:16 +0200

    fix: fixed getDate funct

--- src/helpers/getDate.js
+++ src/helpers/getDate.js
@@ -2,14 +2,14 @@ export function getDate(d) {
     let day, month, year, result, dateSplitted;
 
     result = d.match("[0-9]{2}([\-/ \.])[0-9]{2}[\-/ \.][0-9]{4}");
-    if (null != result) {
+    if (result) {
         dateSplitted = result[0].split(result[1]);
         day = dateSplitted[0];
         month = dateSplitted[1];
         year = dateSplitted[2];
     } else {
         result = d.match("[0-9]{4}([\-/ \.])[0-9]{2}[\-/ \.][0-9]{2}");
-        if (null != result) {
+        if (result) {
             dateSplitted = result[0].split(result[1]);
             day = dateSplitted[2];
             month = dateSplitted[1];
