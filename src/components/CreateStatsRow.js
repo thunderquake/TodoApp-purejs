b14246572910b67f9e52f89517b66a1522ddbc9f
Dmytriieva Anna <loooverrr12@gmail.com>
2024-02-29 18:59:10 +0200

    fix: move functs to separate files

---
+++ src/components/CreateStatsRow.js
@@ -0,0 +1,40 @@
+import { iconElementRender } from "./IconsButton.js";
+import { howMany } from "../helpers/manageStats.js";
+
+export function CreateStatsRow(category) {
+  const tr = document.createElement("tr");
+  tr.classList.add(
+    "rounded-3xl",
+    "bg-white/50",
+    "hover:bg-stone-50/70",
+    "break-words"
+  );
+  tr.setAttribute("id", "stats-tr");
+
+  const iconTd = document.createElement("td");
+  const iconElement = iconElementRender(category);
+  iconElement.classList.add("w-20", "mx-auto");
+  iconTd.appendChild(iconElement);
+  tr.appendChild(iconTd);
+
+  const categoryTd = document.createElement("td");
+  categoryTd.textContent = category;
+  categoryTd.classList.add(
+    "text-left",
+    "pr-8",
+    "max-w-40",
+    "text-pretty",
+    "py-4"
+  );
+  tr.appendChild(categoryTd);
+
+  const notArchivedTd = document.createElement("td");
+  notArchivedTd.textContent = howMany(false, category);
+  tr.appendChild(notArchivedTd);
+
+  const archivedTd = document.createElement("td");
+  archivedTd.textContent = howMany(true, category);
+  tr.appendChild(archivedTd);
+
+  return tr;
+}
---
+++ src/helpers/manageStats.js
@@ -0,0 +1,24 @@
+import { getTableData } from "../helpers/storingData.js";
+import { CATEGORIES } from "../constants/constants.js";
+import { CreateStatsRow } from "../components/CreateStatsRow.js";
+
+export function howMany(isArchivedValue, category) {
+  const allTodos = getTableData();
+  return allTodos.reduce((counter, obj) => {
+    if (obj.category === category && obj.isArchived === isArchivedValue) {
+      counter += 1;
+    }
+    return counter;
+  }, 0);
+}
+
+export function refreshStats() {
+  const tbody = document.getElementById("stats-tbody");
+
+  tbody.innerHTML = "";
+
+  const rows = CATEGORIES.forEach((category) => {
+    const row = CreateStatsRow(category);
+    tbody.appendChild(row);
+  });
+}
