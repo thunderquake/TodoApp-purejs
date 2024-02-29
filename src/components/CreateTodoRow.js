e8362f9f672237ae394f919aadbc88a6981b7b93
Dmytriieva Anna <loooverrr12@gmail.com>
2024-02-29 13:12:17 +0200

    fix: fix table styling and consts

--- src/components/CreateTodoRow.js
+++ src/components/CreateTodoRow.js
@@ -6,24 +6,39 @@ export function addTodoRow(obj) {
   const idRow = obj.id;
 
   const tr = document.createElement("tr");
-  tr.classList.add("rounded-3xl", "bg-white/50");
+  tr.classList.add(
+    "rounded-3xl",
+    "bg-white/50",
+    "hover:bg-stone-50/70",
+    "break-words"
+  );
   tr.setAttribute("id", idRow);
 
   const iconElement = iconElementRender(obj.category);
   addTd(iconElement, tr);
+  iconElement.classList.add("w-20");
 
   const { isArchived, id, contents, ...renderedProperties } = obj;
 
   Object.values(renderedProperties).forEach((value) => {
     const td = document.createElement("td");
-    td.classList.add("text-left");
+    td.classList.add("text-left", "pr-8", "max-w-40", "text-pretty", "py-4");
     td.textContent = value;
     tr.appendChild(td);
   });
 
   const contentsTd = document.createElement("td");
-  contentsTd.classList.add("text-left");
-  contentsTd.textContent = contents;
+  contentsTd.classList.add("text-left", "pr-6", "w-96", "max-w-96");
+  const text = document.createElement("p");
+  text.textContent = contents;
+  contentsTd.appendChild(text);
+  text.classList.add(
+    "text-black",
+    "max-w-96",
+    "break-all",
+    "text-pretty",
+    "py-4"
+  );
   tr.appendChild(contentsTd);
 
   const datesArr = getDate(contents);
@@ -44,7 +59,7 @@ export function addTodoRow(obj) {
 
 function addTd(value, tr) {
   const td = document.createElement("td");
-  td.classList.add("pl-6", "text-left");
-  td.append(value);
+  td.classList.add("py-6", "text-left");
+  td.appendChild(value);
   tr.appendChild(td);
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,21 @@
 export const CATEGORIES = ["idea", "quote", "task", "thoughts"];
+
+export const HEADERS = [
+  "",
+  "Category",
+  "Name",
+  "Date created",
+  "Contents",
+  "Dates",
+  " ",
+];
+
+export const HEADER_WIDTHS = [
+  "w-24",
+  "w-32",
+  "w-48",
+  "w-40",
+  "w-96",
+  "w-40",
+  "w-48",
+];
